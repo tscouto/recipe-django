95ea2a3f355f0d63f9edc8e2ddf3ad7fc5ff1bc3
tscouto <tiago.dscouto@hotmail.com>
2026-01-28 11:24:00 -0300

    tirei o texto defundo

--- project/settings.py
+++ project/settings.py
@@ -126,11 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATIC_ROOT = BASE_DIR / 'static'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "base_static",
 ]
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
