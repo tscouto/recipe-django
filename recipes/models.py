b3f429c46f1708b46d85be5bd3f899e1447779e8
tscouto <tiago.dscouto@hotmail.com>
2026-01-31 22:01:44 -0300

    create side menu

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
 from email.policy import default
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ValidationError
 from django.urls import reverse
 from django.utils.text import slugify
 from django.db.models.functions import Concat
@@ -60,6 +62,22 @@ class Recipe(models.Model):
            slug = f'{slugify(self.title)}'
            self.slug = slug
         return super().save(*args, **kwargs)
+    
+    def clean(self, *args, **kwargs):
+        error_messages = defaultdict(list)
+
+        recipe_from_db = Recipe.objects.filter(
+            title__iexact=self.title
+        ).first()
+
+        if recipe_from_db:
+            if recipe_from_db.pk != self.pk:
+                error_messages['title'].append(
+                    'Found recipes with the same title'
+                )
+
+        if error_messages:
+            raise ValidationError(error_messages)
 
 # Create your models here.
 
